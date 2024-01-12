d493f51689f4c2f589ceb4d32a9e1a9c296de8cf
David Massey <dapmassey@gmail.com>
2024-01-12 14:32:07 +0000

    Add Apollo client and GraphQL query to get post by URI

--- pages/[uri].js
+++ pages/[uri].js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Footer from '../components/Footer'
 import { getPostByUri } from '../lib/test-data';
+import { client } from '../lib/apollo';
+import { gql } from '@apollo/client';
 
 export default function SlugPage({ post }) {
 
@@ -26,18 +28,40 @@ export default function SlugPage({ post }) {
 
     </div>
   )
-}
+};
 
 
-export async function getStaticProps({ params }){
-  const response = await getPostByUri(params.uri)
+export async function getStaticProps({ params }) {
+  const GET_POST_BY_URI = gql`
+      query GetPostByURI($id: ID!) {
+        post(id: $id, idType: URI) {
+          title
+          content
+          date
+          uri
+          author {
+            node {
+              firstName
+              lastName
+            }
+          }
+        }
+      }
+  `;
+
+  const response = await client.query({
+    query: GET_POST_BY_URI,
+    variables: {
+      id: params.uri
+    }
+  })
   const post = response?.data?.post
   return {
     props: {
       post
     }
   }
-}
+};
 
 export async function getStaticPaths(){
     const paths = []
